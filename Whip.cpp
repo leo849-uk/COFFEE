71ce57b97561abb45b5ed5494121f633ec690048
leo849-uk <72377675+leo849-uk@users.noreply.github.com>
2020-10-08 08:46:42 +0700

    Update Whip.cpp

---
+++ Whip.cpp
@@ -0,0 +1,15 @@
+  
+#include "whip.h"
+
+Whiskey::Whiskey( Beverage *b) : CondimentDecorator (b)
+{
+
+}
+
+double Whiskey::cost() {
+    return 54.0 + beverage->cost();
+}
+
+QString Whiskey::getDescription() {
+    return beverage->getDescription() + ", Whiskey";
+}
