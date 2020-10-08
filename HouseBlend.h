43ec1eb20d3b0a7fbe9a79eea0ec6e8e1291df5c
leo849-uk <72377675+leo849-uk@users.noreply.github.com>
2020-10-08 09:05:12 +0700

    Update HouseBlend.h

--- HouseBlend.h
+++ HouseBlend.h
@@ -1,10 +1,12 @@
-#ifndef __HOUSEBLEND_H
-#define __HOUSEBLEND_H
+#ifndef HOUSEBLEND_H
+#define HOUSEBLEND_H
 
 #include "beverage.h"
 
 class HouseBlend : public Beverage {
-      public:
-             HouseBlend();
-             virtual void double cost();
-      }
+public:
+    HouseBlend();
+    virtual double cost();
+};
+
+#endif
